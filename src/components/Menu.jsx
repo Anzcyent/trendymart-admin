6324bd3a18436a78dd2b5ff213e3edfd5866ee96
Cihan <ozcancihan714@gmail.com>
2023-07-23 14:09:36 +0300

    Menu routes

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-
 import menuItems from "../utils/menuItems";
+import { useNavigate } from "react-router-dom";
 
 const Menu = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col lg:items-start items-center bg-darkBlue lg:w-[10%] md:w-[15%] w-[20%] p-3 font-urbanist">
       {menuItems.map((menuItem) => (
@@ -13,7 +14,9 @@ const Menu = () => {
           {menuItem.items.map((item) => (
             <li
               key={item.id}
-              className="text-white flex items-center text-sm mb-1 p-1 cursor-pointer hover:opacity-70 active:scale-95"
+              onClick={!item.disabled ? () => navigate(item.routeUrl) : null}
+              className="text-white flex items-center text-sm mb-1 p-1 hover:opacity-70 active:scale-95"
+              style={{cursor: item.disabled ? "not-allowed" : "pointer"}}
             >
               {item.icon}
               <span className="max-[1050px]:hidden">{item.name}</span>
